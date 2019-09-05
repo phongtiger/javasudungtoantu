1fd12ff1a094fe9dab58daf2d377793bc4be34c7
TuVanPhong <phongken94@gmail.com>
2019-09-05 21:36:33 +0700

    tai len

---
+++ src/main/java/RectangleProgram.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class RectangleProgram {
+    public static void main(String[] args) {
+        float width;
+        float height;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Width:  ");
+        width = scanner.nextFloat();
+
+        System.out.println("Enter Height:  ");
+        height = scanner.nextFloat();
+
+        float area = width*height;
+        System.out.println("Area is: "+area);
+    }
+}
